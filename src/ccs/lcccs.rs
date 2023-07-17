b91b68fba952050474fee73d9f93c2a45275e5f2
CPerezz <c.perezbaro@gmail.com>
2023-07-17 16:12:31 +0200

    change: First LCCCS update
    Removes CCS & CCS_MLEs from the struct as well as renaming some
    variables.
    Aside from that, the functions are being modified so that they always
    get the removed fields as refs.

--- src/ccs/lcccs.rs
+++ src/ccs/lcccs.rs
@@ -1,5 +1,5 @@
 use super::util::{compute_sum_Mz, VirtualPolynomial};
-use super::{CCSWitness, CCS};
+use super::{CCCSInstance, CCSWitness, CCS};
 use crate::ccs::util::compute_all_sum_Mz_evals;
 use crate::hypercube::BooleanHypercube;
 use crate::spartan::math::Math;
@@ -33,40 +33,38 @@ use std::sync::Arc;
 #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
 #[serde(bound = "")]
 pub struct LCCCS<G: Group> {
-  pub(crate) C: Commitment<G>,
+  pub(crate) w_comm: Commitment<G>,
   pub(crate) x: Vec<G::Scalar>,
   pub(crate) u: G::Scalar,
   pub(crate) v: Vec<G::Scalar>,
   // Random evaluation point for the v_i
   pub r_x: Vec<G::Scalar>,
-  // This should not need to be here. Should be a reference only.
-  pub(crate) matrix_mles: Vec<MultilinearPolynomial<G::Scalar>>,
-  pub(crate) ccs: CCS<G>,
+  pub(crate) z: Vec<G::Scalar>,
 }
 
 impl<G: Group> LCCCS<G> {
   // XXX: Double check that this is indeed correct.
   /// Samples public parameters for the specified number of constraints and variables in an CCS
-  pub fn commitment_key(&self) -> CommitmentKey<G> {
-    let total_nz = self.ccs.M.iter().fold(0, |acc, m| acc + m.coeffs().len());
+  // pub fn commitment_key(&self) -> CommitmentKey<G> {
+  //   let total_nz = self.ccs.M.iter().fold(0, |acc, m| acc + m.coeffs().len());
 
-    G::CE::setup(b"ck", max(max(self.ccs.m, self.ccs.t), total_nz))
-  }
+  //   G::CE::setup(b"ck", max(max(self.ccs.m, self.ccs.t), total_nz))
+  // }
 
   /// Checks if the CCS instance is satisfiable given a witness and its shape
-  pub fn is_sat(&self, ck: &CommitmentKey<G>, W: &CCSWitness<G>) -> Result<(), NovaError> {
+  pub fn is_sat(
+    &self,
+    ccs: &CCS<G>,
+    ccs_m_mle: &[MultilinearPolynomial<G::Scalar>],
+    ck: &CommitmentKey<G>,
+  ) -> Result<(), NovaError> {
+    let w = &self.z[(1 + ccs.l)..];
     // check that C is the commitment of w. Notice that this is not verifying a Pedersen
     // opening, but checking that the Commmitment comes from committing to the witness.
-    let comm_eq = self.C == CE::<G>::commit(ck, &W.w);
+    let comm_eq = self.w_comm == CE::<G>::commit(ck, w);
 
-    // check CCS relation
-    let z: Vec<G::Scalar> = [vec![self.u], self.x.clone(), W.w.to_vec()].concat();
-    let computed_v =
-      compute_all_sum_Mz_evals::<G>(&self.matrix_mles, &z, &self.r_x, self.ccs.s_prime);
+    let computed_v = compute_all_sum_Mz_evals::<G>(ccs_m_mle, w, &self.r_x, ccs.s_prime);
     let vs_eq = computed_v == self.v;
-
-    dbg!(vs_eq);
-    dbg!(comm_eq);
 
     if vs_eq && comm_eq {
       Ok(())
@@ -77,13 +75,19 @@ impl<G: Group> LCCCS<G> {
 
   /// Compute all L_j(x) polynomials
   // Can we recieve the MLE of z directy?
-  pub fn compute_Ls(&self, z: &Vec<G::Scalar>) -> Vec<VirtualPolynomial<G::Scalar>> {
-    let z_mle = dense_vec_to_mle(self.ccs.s_prime, z);
+  pub fn compute_Ls(
+    &self,
+    ccs: &CCS<G>,
+    ccs_m_mle: &[MultilinearPolynomial<G::Scalar>],
+    ck: &CommitmentKey<G>,
+    z: &[G::Scalar],
+  ) -> Vec<VirtualPolynomial<G::Scalar>> {
+    let z_mle = dense_vec_to_mle(ccs.s_prime, z);
 
-    let mut vec_L_j_x = Vec::with_capacity(self.ccs.t);
-    for M_j in self.matrix_mles.iter() {
+    let mut vec_L_j_x = Vec::with_capacity(ccs.t);
+    for M_j in ccs_m_mle.iter() {
       // Sanity check
-      assert_eq!(z_mle.get_num_vars(), self.ccs.s_prime);
+      assert_eq!(z_mle.get_num_vars(), ccs.s_prime);
 
       let sum_Mz = compute_sum_Mz::<G>(M_j, &z_mle);
       let sum_Mz_virtual = VirtualPolynomial::new_from_mle(&Arc::new(sum_Mz), G::Scalar::ONE);
@@ -105,7 +109,7 @@ mod tests {
   fn satisfied_ccs_is_satisfied_lcccs_with<G: Group>() {
     // Gen test vectors & artifacts
     let z = CCS::<Ep>::get_test_z(3);
-    let (ccs, witness, instance) = CCS::<Ep>::gen_test_ccs(&z);
+    let (ccs, witness, instance, mles) = CCS::<Ep>::gen_test_ccs(&z);
     let ck = ccs.commitment_key();
     assert!(ccs.is_sat(&ck, &instance, &witness).is_ok());
 
